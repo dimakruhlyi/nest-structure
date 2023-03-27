648f5cfcf7e38da3cdf3e3ef7773c2a680264b5d
Dmytro Kruhlyi <dimakruhly@gmail.com>
2023-03-27 21:13:39 +0300

    users handle logic implementation

---
+++ src/interceptors/serializer.interceptor.ts
@@ -0,0 +1,33 @@
+import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { plainToClass } from "class-transformer";
+
+interface ClassConstructor {
+    new (...args: any[]): {},
+}
+
+export function Serialize(dto: ClassConstructor) {
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: any) {}
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> | Promise<Observable<any>> {
+        // Run something before the request is handled 
+        // by the request handler
+        console.log('Run something before the request is handled:', context);
+
+        return next.handle().pipe(
+            map((data: any) => {
+                // Run something before the response is sent out
+                console.log('Run something before the response is sent out:', data);
+               
+                return plainToClass(this.dto, data, {
+                    excludeExtraneousValues: true,
+                });
+            })
+        )
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Patch, Param, Query, Delete, NotFoundException, ClassSerializerInterceptor } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
+import { Serialize } from 'src/interceptors/serializer.interceptor';
+import { UserDto } from './dtos/user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
+  }
+
+//   @Serialize(UserDto)
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    console.log('Handler is running.')
+    const user = await this.usersService.findOne(parseInt(id));
+    if(!user) {
+        throw new NotFoundException('user not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+}
